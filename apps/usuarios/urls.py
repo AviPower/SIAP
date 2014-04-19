3580fb24b65ae72292ca3ce50403860bae0a7dda
AviPower <avi.alvarenga5@gmail.com>
2014-04-19 19:00:57 -0400

    Agregado  Cambio de password de usuario

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,11 +1,14 @@
 __author__ = 'alvarenga'
 from django.conf.urls import patterns, include, url
-from .views import Usuario, ListUser, AddUser, NewPass
+from .views import Usuario, ListUser, AddUser, ListUserper, AddUserper, password_change
 urlpatterns = patterns('',
     url(r'^$', Usuario.as_view(),name='usuario'),
     url(r'^add/$', AddUser.as_view(),name='usuario_add'),
     url(r'^user/$', ListUser.as_view(),name='usuario_user'),
-    url(r'^nuevo_pass/$', NewPass.as_view(),name='usuario_pass'),
+    url(r'^nuevo_pass/$','apps.usuarios.views.password_change'),
+        url(r'^nuevo_pass/done/$', 'apps.usuarios.views.password_change_done', name='password_change_done'),
+    url(r'^addper/$', AddUserper.as_view(),name='usuario_addper'),
+    url(r'^userper/$', ListUserper.as_view(),name='usuario_userper'),
 
 
 )
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,6 +1,12 @@
-from django.views.generic import CreateView, TemplateView, ListView
-from django.core.urlresolvers import reverse_lazy
-from apps.inicio.models import Perfiles
+from django.views.generic import FormView, TemplateView, ListView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import  PasswordChangeForm
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.template.response import TemplateResponse
+from django.shortcuts import resolve_url
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.csrf import csrf_protect
 
 
 
@@ -13,5 +19,51 @@ class ListUser(TemplateView):
 class AddUser(TemplateView):
     template_name = 'usuarios/user/add.html'
 
-class NewPass(TemplateView):
-    template_name = 'usuarios/cambiar_pass.html'
+class ListUserper(TemplateView):
+    template_name = 'usuarios/user/userper.html'
+
+class AddUserper(TemplateView):
+    template_name = 'usuarios/user/addper.html'
+
+@sensitive_post_parameters()
+@csrf_protect
+@login_required
+def password_change(request,
+                    template_name='usuarios/cambiar_pass.html',
+                    post_change_redirect=None,
+                    password_change_form=PasswordChangeForm,
+                    current_app=None, extra_context=None):
+    """
+    Verifica que no halla ningun error al cambiar el password
+    """
+    if post_change_redirect is None:
+        post_change_redirect = reverse('password_change_done')
+    else:
+        post_change_redirect = resolve_url(post_change_redirect)
+    if request.method == "POST":
+        form = password_change_form(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(post_change_redirect)
+    else:
+        form = password_change_form(user=request.user)
+    context = {
+        'form': form,
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return TemplateResponse(request, template_name, context,
+                            current_app=current_app)
+
+@login_required
+def password_change_done(request,
+                         template_name='usuarios/user/cambiar_pass_done.html',
+                         current_app=None, extra_context=None):
+    """
+    Redirecciona una vez siguiente pagina confirmando el cambio exitoso de password
+    """
+    context = {}
+    if extra_context is not None:
+        context.update(extra_context)
+    return TemplateResponse(request, template_name, context,
+                            current_app=current_app)
