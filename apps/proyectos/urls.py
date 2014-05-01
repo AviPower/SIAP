22254f61eebea5bb00a235805e0598af177fca28
AviPower <avi.alvarenga5@gmail.com>
2014-04-30 22:50:23 -0400

    Formulario Modificacion de Proyecto

--- apps/proyectos/urls.py
+++ apps/proyectos/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import patterns, url
 from django.contrib import admin
 admin.autodiscover()
 from views import listar_proyectos,registrar_proyecto, RegisterSuccessView, RegisterFailedView, detalle_proyecto, buscar_proyecto
-
+from views import editar_proyecto
 urlpatterns = patterns('',
         url(r'^registrar/$',registrar_proyecto, name='registrar_proyecto'),
         #Administracion de Proyectos
         url(r'^$',listar_proyectos, name='list_proyecto'),
         url(r'^(?P<id_proyecto>\d+)$', detalle_proyecto, name='detalle_proyecto'),
         url(r'^search/$',buscar_proyecto, name='buscar_proyectos'),
-        #url(r'^modificar/(?P<id_proyecto>\d+)$', 'proyectos.views.editar_proyecto'),
+        url(r'^modificar/(?P<id_proyecto>\d+)$', editar_proyecto, name='edit_proyecto'),
         #url(r'^cambiarEstado/(?P<id_proyecto>\d+)$', 'proyectos.views.cambiar_estado_proyecto'),
         #url(r'^importar/(?P<id_proyecto>\d+)$', 'proyectos.views.importar_proyecto'),
         #url(r'^equipo/(?P<id_proyecto>\d+)$', 'proyectos.views.ver_equipo'),
