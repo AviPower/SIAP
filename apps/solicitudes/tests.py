85c130cbd90ed911975743845ab7b0c57834c692
msekatcheff <sekatcheff@gmail.com>
2014-06-20 21:18:50 -0400

    Se actualizan las pruebas para items y solicitudes

--- apps/solicitudes/tests.py
+++ apps/solicitudes/tests.py
@@ -4,10 +4,12 @@ from lib2to3.fixer_util import p1
 __author__ = 'marcel'
 
 from django.test import TestCase, Client
-from django.contrib.auth.models import User
-from apps.inicio.models import Perfiles
 from apps.solicitudes.models import Solicitud
-
+from apps.fases.models import Fase
+from apps.proyectos.models import Proyecto
+from django.contrib.auth.models import User
+from apps.items.models import Item
+from apps.tiposDeItem.models import TipoItem
 
 
 class SolicitudTest(TestCase):
@@ -15,9 +17,31 @@ class SolicitudTest(TestCase):
 
     def setUp(self):
         print "\n-----------------TEST SOLICITUD DE CAMBIO------------------------------"
+        user = User.objects.create(username='avelinaaa', first_name='runJoey', last_name='passit',
+                                       password='fija')
+
+        proyecto = Proyecto.objects.create(nombre='prueba', descripcion="Este es un proyecto",
+                                           fecha_ini="2014-01-12",
+                                           fecha_fin="2014-01-14",
+                                           lider= user,
+                                           observaciones="esta es una observacion")
+
+        faseprueba = Fase.objects.create(nombre='faseprueba', descripcion="Este es una fase",
+                            maxItems = 3,  fInicio="2014-01-12", orden = 3,
+                            fCreacion="2014-01-10", proyecto = proyecto)
+        faseprueba.save()
+
+
+        tipoIt =TipoItem.objects.create(nombre="detalles", descripcion="aa")
+        item = Item.objects.create(nombre="itemprueba", descripcion="aa", costo =3, tiempo= 3,
+                            estado= 'PEN', version= 1, fecha_creacion = '2014-05-12', fecha_mod= '2014-05-15',
+                            tipo_item = tipoIt, fase = faseprueba
+                            )
+
 
         Solicitud.objects.create(nombre='solici', descripcion="Esta es una solicitud",
-                                           fecha_solicitud="2014-01-12")
+                                           fecha="2014-01-12", proyecto = proyecto, item = item,
+                                           costo = 3, tiempo = 4, usuario = user)
 
         print("Creo la solicitud  mediante el metodo setUp()")
 
